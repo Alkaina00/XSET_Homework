e31505ba0afeffd9420b5f9130cb3c46d99949d1
Alina Gainulina <alina.gajnullina2015@yandex.ru>
2025-07-08 17:16:25 +0500

    Homework 22 add logic replacing letters

--- src/homework22/BufferedReaderWriter.java
+++ src/homework22/BufferedReaderWriter.java
@@ -4,19 +4,41 @@ import java.io.*;
 
 public class BufferedReaderWriter {
     public static void main(String[] args){
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter("bufferreaderwriter.txt"))){
-            String text = "Hello World! \nHey! Teachers! Leave the kids alone";
-            bw.write(text);
-        }
-        catch (IOException ex){
-            System.out.println(ex.getMessage());
-        }
-
-        try(BufferedReader br = new BufferedReader(new FileReader("bufferreaderwriter.txt"))){
+        try(BufferedReader br = new BufferedReader(new FileReader("introduce.txt"))){
             String line;
+            StringBuilder result = new StringBuilder();
+            String vowels = "аеёиоуыэюяАЕЁИОУЫЭЮЯ";
+
+            int count_vowels = 0;
+            int count_consonants = 0;
+
             while((line=br.readLine()) != null){
-                System.out.println(line);
+                for(char c: line.toCharArray()){
+                    //Проверяем, является ли символ буквой
+                    if(Character.isLetter(c)){
+                        //Проверяем, буква гласная?
+                        if(vowels.indexOf(c) != -1){
+                            result.append("а"); //Если да, то меняем на а
+                            count_vowels++;
+                        }
+                        else {
+                            result.append("м"); //Если нет, то меняем на м
+                            count_consonants++;
+                        }
+                    }
+                    else result.append(c); // добавляем символы, которые не являются буквой, без изменений
+                }
+                result.append("\n"); // добавляем перенос строки
             }
+            System.out.println(result);
+            try(BufferedWriter bw = new BufferedWriter(new FileWriter("result_homework22.txt"))){
+                bw.write(String.valueOf(result));
+            }
+            catch (IOException ex){
+                System.out.println(ex.getMessage());
+            }
+            System.out.println("Количество гласных: " + count_vowels);
+            System.out.println("Количество согласных: " + count_consonants);
         }
         catch (IOException ex){
             System.out.println(ex.getMessage());
